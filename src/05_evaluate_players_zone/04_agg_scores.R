8d47db892cd833bb2b012016a2f4ec6689a53261
James Venzor <James@Jamess-MBP-3.home>
2020-12-18 12:49:13 -0500

    adding R scriptS

--- src/05_evaluate_players_zone/04_agg_scores.R
+++ src/05_evaluate_players_zone/04_agg_scores.R
@@ -52,6 +52,45 @@ player_tackling_skills = read.csv("~/Desktop/CoverageNet/src/05_evaluate_players
 pass_attempt_epa = read.csv("~/Desktop/CoverageNet/src/03_coverageNet/02_score_attempt/outputs/pass_attempt_epa_data.csv")
 pass_arrived_epa = read.csv("~/Desktop/CoverageNet/src/03_coverageNet/01_score_arrived/outputs/pass_arrived_epa_data.csv")
 
+man_zone_classification = rbind(
+  read.csv("~/Desktop/CoverageNet/src/01_identify_man_coverage/outputs/all_positions_pass_attempts_man_zone_classes.csv") %>%
+    dplyr::select(gameId, playId, nflId, zone_probability),
+  read.csv("~/Desktop/CoverageNet/src/01_identify_man_coverage/outputs/all_positions_sacks_man_zone_classes.csv") %>%
+    dplyr::select(gameId, playId, nflId, zone_probability)
+) %>%
+  arrange(gameId, playId, nflId) %>%
+  distinct(gameId, playId, nflId, .keep_all = TRUE)
+
+man_coverage = read.csv("~/Desktop/CoverageNet/src/01_identify_man_coverage/outputs/man_defense_off_coverage_assignments_all_lbs.csv")
+
+wr_db_zone_matchups_tot = read.csv("~/Desktop/CoverageNet/src/01_identify_man_coverage/outputs/zone_defense_off_coverage_assignments_all_lbs.csv")
+
+
+# Filtering to Players In Man Coverage ------------------------------------
+
+# we take anyone who wasn't identified in man and make them zone
+zone_coverage = man_zone_classification %>%
+  anti_join(man_coverage,
+            by = c("gameId", "playId", "nflId" = "nflId_def"))
+
+zone_coverage_counts = zone_coverage %>%
+  ungroup() %>%
+  distinct(gameId, playId, nflId) %>%
+  group_by(nflId) %>%
+  summarize(zone_plays = n()) %>%
+  arrange(desc(zone_plays)) %>%
+  rename(nflId_def = nflId) %>%
+  inner_join(players %>%
+               dplyr::select(nflId, displayName, position) %>%
+               rename(nflId_def = nflId))
+
+zone_coverage_cover_counts = wr_db_zone_matchups_tot %>% 
+  ungroup() %>%
+  distinct(gameId, playId, nflId) %>%
+  group_by(nflId) %>%
+  summarize(zone_cover_plays = n()) %>%
+  arrange(desc(zone_cover_plays)) %>%
+  rename(nflId_def = nflId)
 
 # Getting On Percentage of Throws that Are on Target ---------------------------
 
@@ -69,12 +108,14 @@ on_target_comp_perc = (plays %>%
 
 # Joining the Tables, and Summarizing -------------------------------------
 
-skills_table = player_closing_skills %>%
-              dplyr::select(-targets, -qualifying) %>%
+skills_table = zone_coverage_counts %>%
+  full_join(zone_coverage_cover_counts) %>%
+  full_join(player_closing_skills %>%
+              dplyr::select(-targets, -qualifying, -displayName, -position)) %>%
   full_join(player_ball_skills %>%
-              dplyr::select(-position, -qualifying)) %>%
+              dplyr::select(-position, -qualifying, -displayName)) %>%
   full_join(player_tackling_skills %>%
-              dplyr::select(-position, -qualifying))
+              dplyr::select(-position, -qualifying, -displayName))
 
 skills_table[is.na(skills_table)] = 0
  
@@ -86,7 +127,7 @@ skills_table = skills_table %>%
          eps_zone_coverage_no_tackling = eps_zone_coverage - eps_tackling,
          closing_penalties = closing_penalties,
          ball_skills_penalties = ball_skills_penalties) %>%
-  dplyr::select("position", "displayName", "nflId_def", "eps_zone_coverage","targets",
+  dplyr::select("position", "displayName", "nflId_def", "eps_zone_coverage","zone_plays", "zone_cover_plays", "targets",
                 "completions", "PB", "ball_hawk_pbus", "ball_hawk_ints", "interceptions","Tackles", "FF", 
                 "closing_penalties","ball_skills_penalties","eps_saved_closing_w_penalties",
          "eps_saved_ball_skills_w_penalties","eps_tackling", "eps_int_returns", "eps_ball_hawk",
